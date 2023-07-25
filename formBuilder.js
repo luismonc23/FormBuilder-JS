b210372fa0ab3ece4b461705e89b6974ff78b937
luismonc23 <35046829+luismonc23@users.noreply.github.com>
2023-07-25 15:48:17 -0600

    File upload
    OD: 12/22/2022

---
+++ formBuilder.js
@@ -0,0 +1,238 @@
+(function(global, $) {
+    class Field{
+        constructor(name, label, type, order, validation, disabled, apply, width, value, slots, options) {
+            this.name = name;
+            this.label = label;
+            this.type = type;
+            this.order = order;
+            this.validation = validation;
+            this.disabled = disabled;
+            this.apply = apply;
+            this.width = width;
+            this.value = value;
+            this.slots = slots;
+            this.options = options;
+            //field.customProps = "onkeyup='' onchange=''";
+
+            // column in which the field is placed
+            // options for the radio and select
+            //placeholder
+            //prepend or append html content
+            //custom props
+            //default value
+            //Some kind of callback like ajax
+            //file allowed extensions
+        }
+
+        getName() { return this.label; }
+        getLabel() { return this.label; }
+        getType() { return this.type; }
+        getOrder() { return this.order; }
+        getValidation() { return this.validation; }
+        getDisabled() { return this.disabled; }
+        getApply() { return this.apply; }
+        getWidth() { return this.width; }
+        getValue() { return this.value; }
+        getSlots() { return this.slots; }
+        //static getFromArray()
+    }
+    class Form{
+        constructor(fields){
+            this.validateFieldsConfig(fields);
+            this.fields = fields.map(({
+                name, 
+                label, 
+                type, 
+                order, 
+                validation, 
+                disabled, 
+                apply, 
+                width = null, 
+                value = null, 
+                slots = [], 
+                options = []}) => new Field(
+                    name, label, type, order, validation, disabled, apply, width, value, slots, options));
+        }
+        
+        //create form from a jquery element
+        getFields(){ return this.fields; }
+
+        validateFieldsConfig(fields){
+            for (let field of fields){
+                if(typeof field.name !== 'string'){
+                    throw new Error(`Invalid type for 'name' field: ${typeof field.name}`);
+                }
+                if(typeof field.label !== 'string'){
+                    throw new Error(`Invalid type for 'label' field: ${typeof field.label}`);
+                }
+                if(typeof field.type !== 'string'){
+                    throw new Error(`Invalid type for 'type' field: ${typeof field.type}`);
+                }
+                // Other validations
+            }
+        }
+    }
+    var FormBuilder = function() {
+
+    };
+    FormBuilder.prototype.validate = function(form) {
+
+    }
+    FormBuilder.prototype.validateHTML = function(html) {
+        
+    }
+    FormBuilder.prototype.destroy = function(target) {
+        $(target).empty();
+    }
+    FormBuilder.prototype.transformRadioOptions = function(radioClassName) {
+        return function(option) { 
+            return `<input type="radio" id="${option.name}-option" class="${radioClassName} ${option.className ?? ''}" onchange="${option.onChange}" ${field.checked ? "checked" : ""}>
+            <label for="${option.name}-option">${option.label}</label>
+            <br>`;
+        }
+    }
+    FormBuilder.prototype.build = function(target, fields) {
+        // add params: columns, name (for className also)
+        //if ($) {
+        debugger;
+        try {
+            const form = new Form(fields);
+            const fieldsList = form.getFields();
+            let html = "";
+            let columns = 2;
+            let maxRows = 6;
+            let currentRow = 0;
+
+            // Init form
+            let colspan = 12/columns;
+            //html = `<div class="col-sm-${colspan}">`;
+
+            fieldsList.forEach(function(field, index, array) {
+                if(currentRow == 0 && index !== array.length - 1){
+                    html += `<div class="col-sm-${colspan}">`;
+                    //currentRow++; //starts in 1
+                }
+                if(field.apply){
+                    switch(field.type) {
+                        case 'text':case 'password':case 'number':
+                            html += `<div class="row" id="${field.name}-container">
+                                        <div class="col-sm-${field.width}">
+                                            <span id="${field.name}-label">${field.label}</span>
+                                            <input type="${field.type}" class="form-control ${field.className}" id="${field.name}" value="${field.value}">
+                                            ${field.slots?.columnEnd ?? ""}
+                                        </div>
+                                    </div><br>`;
+                            currentRow++;
+                            break;
+                            
+                        case 'file':
+                            html += `<div class="row" id="${field.name}-container">
+                                        <div class="col-sm-12">
+                                            <span>${field.label}</span>
+                                            <input type="file" id="${field.name}">
+                                            ${field.slots?.columnEnd ?? ""}
+                                        </div>
+                                    </div><br>`;
+                            currentRow++;
+                            break;
+                            
+                        case 'radio':
+                            html += `<div class="row" id="${field.name}-container">
+                                        <div class="col-sm-12">
+                                            <span>${field.label}</span>
+                                            ${field.options?.map(this.transformRadioOptions(field.name + "-radio"))}
+                                        </div>
+                                    </div><br>`;
+                            currentRow++;
+                            break;
+                            
+                        case 'select':
+                            html += `<div class="row" id="${field.name}-container">
+                                        <div class="col-sm-12">
+                                            <span>${field.label}</span>
+                                            <select class="form-control" id="${field.name}" value="${field.value}"></select>
+                                        </div>
+                                    </div><br>`;
+                            currentRow++;
+                            break;
+                            
+                        case 'date':
+                            html += `<div class="row"  id="${field.name}-container">
+                                        <div class="col-sm-12">
+                                            <span>${field.label}</span>
+                                            <div class="input-group date">
+                                                <input type="date" id="${field.name}" class="form-control" value="${field.value}">
+                                            </div>
+                                        </div>
+                                    </div><br>`;
+                            currentRow++;
+                            break;
+                            
+                        case 'textarea':
+                            html += `<div class="row" id="${field.name}-container">
+                                        <div class="col-sm-12">
+                                            <span>${field.label}</span>
+                                            <textarea style="resize: none; height: 12vh;" class="form-control" maxlength="1000" id="${field.name}">${field.value}</textarea>
+                                        </div>
+                                    </div><br>`;
+                            currentRow++;
+                            break;
+                            
+                        case 'button':
+                            html += `<div class="row" id="${field.name}-container">
+                                        <div class="col-sm-12">
+                                            button
+                                        </div>
+                                    </div><br>`;
+                            currentRow++;
+                            break;
+                        case 'custom':
+                            html += `<div class="row" id="${field.name}-container">
+                                        <div class="col-sm-12">
+                                            ${field.customFieldHtml}
+                                        </div>
+                                    </div><br>`;
+                            currentRow++;
+                            break;
+                    }
+                } else {
+                    currentRow++; //Keeps the number of rows consistent
+                    // html += `<div class="row">
+                    //             <div class="col-sm-12">
+                    //             </div>
+                    //         </div><br>`;
+                }
+                if(currentRow == maxRows + 1 || index === array.length - 1){ //Reached max rows or is last element in the array
+                    html += `</div>`;
+                    currentRow = 0;
+                } // this condition outside of the apply if will cause insertion of an empty field
+            });
+            
+            $(target).empty().append(html);
+
+            console.log("Form", form);
+        } catch (e) {
+            console.error("There is a problem with one or more fields", e.message);
+        }
+
+        //} else { //no jquery fallback
+
+        //}
+    };
+
+    global.FormBuilder = FormBuilder; // getting to the global scope
+})(window, window.jQuery || null);
+
+// var fb = new FormBuilder();
+// let fields = [
+//     { name: "file", label: "File", type: "file", order: 0, validation: null, disabled: true, apply: function() { return true } },
+//     { name: "file", label: "File", type: "file", order: 1, validation: null, disabled: true, apply: function() { return true } },
+//     { name: "file", label: "File", type: "file", order: 2, validation: null, disabled: true, apply: function() { return true } },
+//     { name: "file", label: "File", type: "file", order: 3, validation: null, disabled: true, apply: function() { return true } },
+//     { name: "file", label: "File", type: "file", order: 4, validation: null, disabled: true, apply: function() { return true } },
+//     { name: "file", label: "File", type: "file", order: 5, validation: null, disabled: true, apply: function() { return true } },
+//     { name: "file", label: "File", type: "file", order: 6, validation: null, disabled: true, apply: function() { return true } },
+//     { name: "file", label: "File", type: "file", order: 7, validation: null, disabled: true, apply: function() { return true } },
+//     { name: "file", label: "File", type: "file", order: 8, validation: null, disabled: true, apply: function() { return true } },
+// ];
+// fb.build(null, fields);
---
+++ test.js
@@ -0,0 +1,15 @@
+!function (a, b) {
+    "use strict";
+    "function" == typeof define && define.amd ? define(["jquery"], b) : "object" == typeof exports ? module.exports = b(require("jquery")) : a.bootbox = b(a.jQuery)
+}(this, function a(b) {
+    debugger;
+    console.log("Hi");
+
+    p = {};
+
+    p.build = function(){
+
+    }
+
+    $.extend($.ui, { datepicker: { version: "1.10.2" } });
+})
